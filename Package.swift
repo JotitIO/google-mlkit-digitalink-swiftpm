8d337c7fe828a905896e72ab6ad5a3771614e668
Guy Shaviv <guy@shaviv.org>
2026-03-07 15:12:08 +0200

    Add ZipArchive back with correct SPM product name (ZipArchive, not SSZipArchive)

--- Package.swift
+++ Package.swift
@@ -25,6 +25,7 @@ let package = Package(
         .package(url: "https://github.com/google/GoogleUtilities.git", exact: "8.1.0"),
         .package(url: "https://github.com/google/gtm-session-fetcher.git", exact: "3.5.0"),
         .package(url: "https://github.com/firebase/nanopb.git", exact: "2.30910.0"),
+        .package(url: "https://github.com/ZipArchive/ZipArchive.git", exact: "2.6.0"),
     ],
     targets: [
         .binaryTarget(
@@ -52,6 +53,7 @@ let package = Package(
             dependencies: [
                 "MLKitCommon",
                 "GoogleToolboxForMac",
+                .product(name: "ZipArchive", package: "ZipArchive"),
                 .product(name: "FBLPromises", package: "promises"),
                 .product(name: "GoogleDataTransport", package: "GoogleDataTransport"),
                 .product(name: "GULAppDelegateSwizzler", package: "GoogleUtilities"),
